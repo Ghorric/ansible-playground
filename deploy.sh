9a3eedee36ceb24588430b8c2e44ab6e475dc42a
Michael Wengle <michael.wengle@gmail.com>
2018-10-16 22:17:35 +0900

    Exec first cmd on hosts

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker build -t wengle/docker-karaf-ssh docker/docker-karaf-ssh/
 docker save -o ./docker/docker-dind-ansible-playbook/docker-karaf-ssh.dimg wengle/docker-karaf-ssh
 docker build -t wengle/ansible-dind-playbook docker/docker-dind-ansible-playbook/
-sh ./docker/docker-dind-ansible-playbook/scripts/RunServerAnsible.sh sh ./docker/docker-dind-ansible-playbook/scripts/StartDockerDaemonAndAnsible.sh -i "ansible/inventories/hosts-docker.ini" "ansible/playbooks/site.yml" $@
+sh ./docker/docker-dind-ansible-playbook/scripts/RunServerAnsible.sh sh ./docker/docker-dind-ansible-playbook/scripts/StartDockerDaemonAndAnsible.sh -i "ansible/playbooks/inventories/hosts-docker.ini" "ansible/playbooks/site.yml" $@
